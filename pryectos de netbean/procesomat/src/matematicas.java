681b604aab5a1fb6d5a331d8c102effaa34878d0
andrewprogra <carlosa.j456@gmail.com>
2024-07-18 19:37:36 -0600

    modificacion

--- pryectos de netbean/procesomat/src/matematicas.java
+++ pryectos de netbean/procesomat/src/matematicas.java
@@ -1,3 +1,4 @@
+ 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +8,7 @@
  *
  * @author carlo
  */
+
 public class matematicas {
     public static void main(String[] args){
         int primernum=4;
