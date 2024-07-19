8eebe0e9e7da0df8302245bbd97360a00cfbdc05
andrewprogra <carlosa.j456@gmail.com>
2024-07-18 19:11:01 -0600

    modificacion

---
+++ pryectos de netbean/procesomat/src/matematicas.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author carlo
+ */
+public class matematicas {
+    public static void main(String[] args){
+        int primernum=4;
+        int segundonnum=7;
+        
+        int resultado= primernum + segundonnum;
+        
+        System.out.println("primer numero: "+primernum);
+        System.out.println("segundo numero:"+segundonnum);
+        System.out.println("resultado es:"+resultado);
+        
+    }
+}
