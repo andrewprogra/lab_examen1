d7a6bf0d739f213844ee52b85b343633ebce83ac
andrewprogra <carlosa.j456@gmail.com>
2024-07-18 19:49:31 -0600

    modificacion

---
+++ pryectos de netbean/procesomat/src/mates.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author carlo
+ */
+public class mates {
+    public static void main(String[] args){
+        System.out.print("ingresar numero de cuenta");
+        Scanner loli=new Scanner(System.in);
+        
+        System.out.print("Ingresar numero de cuenta:");
+        int cuentaAlumno=loli.nextInt();
+        
+        System.out.print("cuenta de alumno: "+cuentaAlumno);
+        
+    }
+    
+}
